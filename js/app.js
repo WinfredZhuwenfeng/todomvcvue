291d8da18fdb370867a920c0b041990ecf57d110
Winfred <1419657141@qq.com>
2018-07-17 15:31:37 +0800

    完成添加功能

--- js/app.js
+++ js/app.js
@@ -5,11 +5,24 @@
 const app = new Vue({
   el: '#app',
   data: {
+    todotext:'',
     todos: [
     {id: 1, title: 'eating', completed: true},
     {id: 2, title: 'sleeping', completed: false},
     {id: 3, title: 'coding', completed: true},
     ]
+  },
+  methods:{
+    addTodo () {
+      let [ntt, todos, id = 1] = [this.todotext, this.todos]
+      if(ntt.trim().length === 0) return
+      todos.push({
+        id: todos[todos.length - 1].id + 1,
+        title: ntt,
+        complete: false
+      })
+    this.todotext = ''
+    }
   }
 })
 })(window, Vue);
